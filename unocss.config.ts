f79e1f2591e0c9f30c87d0572f589ccb614c1c02
RainbowBird <git@luoling.moe>
2025-05-22 17:58:28 +0800

    chore: unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,4 +1,13 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetAttributify, presetIcons, presetTypography, presetWind3 } from 'unocss'
 
 export default defineConfig({
+  presets: [
+    presetWind3(),
+    presetAttributify(),
+    presetTypography(),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
+  ],
 })
